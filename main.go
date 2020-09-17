14a9ab59c9e8e70780209812f6b2564010668893
Joyce Kung <thejoycekung@gmail.com>
2020-09-17 14:50:56 -0400

    progress: define originUnit

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var originUnit string
@@ -20,16 +21,17 @@ func main() {
 
 	if len(os.Args) != 2 {
 		printError(errInvalidArguments)
-	} else {
-		for {
-			fmt.Print("What is the current temperature in " + originUnit + " ? ")
+	}
 
-			fmt.Print("Would you like to convert another temperature ? (y/n) ")
+	originUnit = strings.ToUpper(os.Args[1])
+	for {
+		fmt.Print("What is the current temperature in " + originUnit + " ? ")
 
-			if shouldConvertAgain != "Y" {
-				fmt.Println("Good bye!")
-				break
-			}
+		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+
+		if shouldConvertAgain != "Y" {
+			fmt.Println("Good bye!")
+			break
 		}
 	}
 }
